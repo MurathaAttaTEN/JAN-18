ec6de85fa96c4a0f639ead7ac9f78323a20df10e
MurathaAttaTEN <attamuratha@gmail.com>
2022-02-18 23:48:29 +0300

    jan 18

---
+++ src/Main.kt
@@ -0,0 +1,37 @@
+fun main(){
+    hello("wanjiru")
+    modulous(30 ,16)
+    sum(10, 20,40, 60)
+
+}
+
+fun hello(name: String){
+    var name = "wanjiru"
+    println("hello "+ name)
+}
+fun  modulous (a: Int,b:Int ){
+    var modulous = a%b
+    println(modulous)
+
+}
+ fun sum (a:Int, b:Int, c:Int, d:Int) {
+     var sum = a+b+c+d
+     println(sum)
+
+
+ }
+fun interesting (){
+    var interesting = "i am wow"
+    println(interesting)
+}
+
+
+
+
+
+
+
+
+
+
+
